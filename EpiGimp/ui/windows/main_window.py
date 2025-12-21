57dd3349f3886e78239cc65e63a9d05bb9969f9b
Tipbs <thibaut.degodenne@epitech.eu>
2025-12-21 22:41:23 +0100

    Remove some corupted features

--- EpiGimp/ui/windows/main_window.py
+++ EpiGimp/ui/windows/main_window.py
@@ -1,9 +1,7 @@
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QStatusBar, QFileDialog, QPushButton
-from PySide6.QtGui import QAction
-from PySide6.QtGui import QKeySequence
+from PySide6.QtGui import QAction, QKeySequence
+from PySide6.QtWidgets import QFileDialog, QMainWindow, QVBoxLayout, QWidget, QStatusBar
 from EpiGimp.ui.widgets.canvas_widgets import CanvasWidget
 from EpiGimp.config.settings import load_settings
-from EpiGimp.ui.widgets.export_widget import ExportWidget
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -17,21 +15,21 @@ class MainWindow(QMainWindow):
 
         self.canvas = CanvasWidget()
         layout.addWidget(self.canvas)
-        self.canvas._drawButton = QPushButton("Draw", self)
-        self.canvas._drawButton.move(10, 10)
-        self.canvas._drawButton.clicked.connect(self.canvas._toggle_drawing_mode)
+        # self.canvas._drawButton = QPushButton("Draw", self)
+        # self.canvas._drawButton.move(10, 10)
+        # self.canvas._drawButton.clicked.connect(self.canvas._toggle_drawing_mode)
         vlayout = QVBoxLayout()
         layout.addLayout(vlayout)
-        vlayout.addWidget(self.canvas._drawButton, 0)
+        # vlayout.addWidget(self.canvas._drawButton, 0)
 
-        self.circle_btn = QPushButton("Circle Tool")
-        self.circle_btn.setCheckable(True)  
-        vlayout.addWidget(self.circle_btn, 2)
-        self.circle_btn.toggled.connect(self.toggle_circle_mode)
-
-        self.select_btn = QPushButton("Select Tool")
-        vlayout.addWidget(self.select_btn, 3)
-        self.select_btn.clicked.connect(self.canvas.enable_selection_mode)
+        # self.circle_btn = QPushButton("Circle Tool")
+        # self.circle_btn.setCheckable(True)  
+        # vlayout.addWidget(self.circle_btn, 2)
+        # self.circle_btn.toggled.connect(self.toggle_circle_mode)
+        #
+        # self.select_btn = QPushButton("Select Tool")
+        # vlayout.addWidget(self.select_btn, 3)
+        # self.select_btn.clicked.connect(self.canvas.enable_selection_mode)
 
         self._create_actions()
         self._create_menus()
@@ -48,19 +46,19 @@ class MainWindow(QMainWindow):
         self.open_act.triggered.connect(self.open_file)
 
 
-        self.save_act = QAction('Save As...', self)
-        self.save_act.setShortcut(QKeySequence('Ctrl+S'))
-        self.save_act.triggered.connect(self.save_file)
-
-        self.export_act = QAction('Export...', self)
-        self.export_act.setShortcut(QKeySequence('Ctrl+E'))
-        self.export_act.triggered.connect(self.export_file)
+        # self.save_act = QAction('Save As...', self)
+        # self.save_act.setShortcut(QKeySequence('Ctrl+S'))
+        # self.save_act.triggered.connect(self.save_file)
+        #
+        # self.export_act = QAction('Export...', self)
+        # self.export_act.setShortcut(QKeySequence('Ctrl+E'))
+        # self.export_act.triggered.connect(self.export_file)
 
     def _create_menus(self):
         file_menu = self.menuBar().addMenu('File')
         file_menu.addAction(self.open_act)
-        file_menu.addAction(self.save_act)
-        file_menu.addAction(self.export_act)
+        # file_menu.addAction(self.save_act)
+        # file_menu.addAction(self.export_act)
 
 
     def open_file(self):
@@ -69,19 +67,19 @@ class MainWindow(QMainWindow):
             self.canvas.load_image(path)
 
 
-    def save_file(self):
-        path, _ = QFileDialog.getSaveFileName(self, 'Save image')
-        if path:
-            self.canvas.save_image(path)
+    # def save_file(self):
+    #     path, _ = QFileDialog.getSaveFileName(self, 'Save image')
+    #     if path:
+    #         self.canvas.save_image(path)
+    #
+    # def export_file(self):
+    #     export_dialog = ExportWidget(self)
+    #     image_data = self.canvas.get_numpy()
+    #     export_dialog.export_image(image_data)
 
-    def export_file(self):
-        export_dialog = ExportWidget(self)
-        image_data = self.canvas.get_numpy()
-        export_dialog.export_image(image_data)
-
-    def toggle_circle_mode(self, checked):
-        self.canvas.set_circle_mode(checked)
-        if checked:
-            self.circle_btn.setText("Circle Mode: ON")
-        else:
-            self.circle_btn.setText("Circle Mode: OFF")
+    # def toggle_circle_mode(self, checked):
+    #     self.canvas.set_circle_mode(checked)
+    #     if checked:
+    #         self.circle_btn.setText("Circle Mode: ON")
+    #     else:
+    #         self.circle_btn.setText("Circle Mode: OFF")
